1a5ac5e74fea96bb10238be2317bf857f0e45a9c
fayro <fayro@FAYROUZGOUMAA>
2026-02-21 00:51:52 +0200

    part1

--- G-NET-42-C#-4/Program.cs
+++ G-NET-42-C#-4/Program.cs
@@ -1,59 +1,59 @@
 ﻿namespace G_NET_42_C__4
 {
-    enum DayOfWeek
-    {
-        Saturday = 1,
-        Sunday,
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday
-    }
+    //enum DayOfWeek
+    //{
+    //    Saturday = 1,
+    //    Sunday,
+    //    Monday,
+    //    Tuesday,
+    //    Wednesday,
+    //    Thursday,
+    //    Friday
+    //}
 
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region Part1
-            int dayNumber = ReadDayNumber();
+            //#region Part1
+            //int dayNumber = ReadDayNumber();
 
-            if (!IsValidDayNumber(dayNumber))
-            {
-                Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
-                return;
-            }
+            //if (!IsValidDayNumber(dayNumber))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+            //    return;
+            //}
 
-            DayOfWeek selectedDay = (DayOfWeek)dayNumber;
+            //DayOfWeek selectedDay = (DayOfWeek)dayNumber;
 
-            PrintDayDetails(selectedDay);
+            //PrintDayDetails(selectedDay);
         }
-        static int ReadDayNumber()
-        {
-            Console.Write("Enter a day number (1–7): ");
-            return int.Parse(Console.ReadLine() ?? "0");
-        }
-        static bool IsValidDayNumber(int number)
-        {
-            return number >= 1 && number <= 7;
-        }
-        static void PrintDayDetails(DayOfWeek day)
-        {
-            Console.WriteLine($"Day: {day}");
+        //static int ReadDayNumber()
+        //{
+        //    Console.Write("Enter a day number (1–7): ");
+        //    return int.Parse(Console.ReadLine() ?? "0");
+        //}
+        //static bool IsValidDayNumber(int number)
+        //{
+        //    return number >= 1 && number <= 7;
+        //}
+        //static void PrintDayDetails(DayOfWeek day)
+        //{
+        //    Console.WriteLine($"Day: {day}");
 
-            switch (day)
-            {
-                case DayOfWeek.Saturday:
-                case DayOfWeek.Sunday:
-                    Console.WriteLine("It's the Weekend");
-                    break;
+        //    switch (day)
+        //    {
+        //        case DayOfWeek.Saturday:
+        //        case DayOfWeek.Sunday:
+        //            Console.WriteLine("It's the Weekend");
+        //            break;
 
-                default:
-                    Console.WriteLine("It's a Workday");
-                    break;
-            }
-        }
-        #endregion
+        //        default:
+        //            Console.WriteLine("It's a Workday");
+        //            break;
+        //    }
+        //}
+        //#endregion
     }
 }
