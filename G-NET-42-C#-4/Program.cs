a8d177bf104d5d3c532b17fa54be6d52c73bef95
fayro <fayro@FAYROUZGOUMAA>
2026-02-21 01:00:40 +0200

    part2

--- G-NET-42-C#-4/Program.cs
+++ G-NET-42-C#-4/Program.cs
@@ -28,7 +28,16 @@
             //DayOfWeek selectedDay = (DayOfWeek)dayNumber;
 
             //PrintDayDetails(selectedDay);
+            //#endregion
+            //---------------------------------------part2-------------
+            //#region part2
+            //int arraySize = ReadArraySize();
+            //int[] numbers = ReadArrayElements(arraySize);
+
+            //DisplayArrayStatistics(numbers);
+            //#endregion
         }
+        //#region part1
         //static int ReadDayNumber()
         //{
         //    Console.Write("Enter a day number (1–7): ");
@@ -55,5 +64,59 @@
         //    }
         //}
         //#endregion
+        //#region part2
+        //static int ReadArraySize()
+        //{
+        //    Console.Write("Enter array size: ");
+        //    return int.Parse(Console.ReadLine() ?? "0");
+        //}
+
+        //static int[] ReadArrayElements(int size)
+        //{
+        //    int[] values = new int[size];
+
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        Console.Write($"Enter element [{i}]: ");
+        //        values[i] = int.Parse(Console.ReadLine() ?? "0");
+        //    }
+
+        //    return values;
+        //}
+        //static void DisplayArrayStatistics(int[] numbers)
+        //{
+        //    int sum = 0;
+        //    int max = numbers[0];
+        //    int min = numbers[0];
+
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        sum += numbers[i];
+
+        //        if (numbers[i] > max)
+        //            max = numbers[i];
+
+        //        if (numbers[i] < min)
+        //            min = numbers[i];
+        //    }
+
+        //    double average = (double)sum / numbers.Length;
+
+        //    Console.WriteLine($"\nSum = {sum}");
+        //    Console.WriteLine($"Average = {average}");
+        //    Console.WriteLine($"Max = {max}");
+        //    Console.WriteLine($"Min = {min}");
+
+        //    Console.Write("Reverse = ");
+        //    for (int i = numbers.Length - 1; i >= 0; i--)
+        //    {
+        //        Console.Write(numbers[i]);
+
+        //        if (i > 0)
+        //            Console.Write(", ");
+        //    }
+        //}
+        //    #endregion
+        
     }
 }
